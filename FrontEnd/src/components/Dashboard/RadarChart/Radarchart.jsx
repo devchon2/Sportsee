b964b03c377d8ad9a6c61c29058d45f9e1276fd4
chon <121806095+devchon2@users.noreply.github.com>
2023-10-16 15:43:46 +0200

    rectification des marges des widget en 1024*768

--- FrontEnd/src/components/Dashboard/RadarChart/Radarchart.jsx
+++ FrontEnd/src/components/Dashboard/RadarChart/Radarchart.jsx
@@ -6,7 +6,7 @@ export default function RadarCharts({ data }) {
 
   return (
     <ResponsiveContainer className={style.radarContainer} width="30%" height="100%" aspect={1}    >
-      <RadarChart className={style.radarChart} cx="50%" cy="50%" data={data} outerRadius='70%'   >
+      <RadarChart className={style.radarChart} cx="50%" cy="50%" data={data} outerRadius='65%'   >
         <PolarGrid className={style.axis} gridType="polygon" radialLines={false} />
         <PolarAngleAxis dataKey='kind' domain={['datamin', 'datamax']} tickLine={false} tickSize={10} />
         <Radar dataKey='data' fill="red" dot={false} fillOpacity={0.7} />
